f1757dd8fe8d17e6dbc2ab0dbad8112fbcd67c5c
Juan Domingo Esteva <juan.d.esteva@gmail.com>
2026-06-29 05:11:08 -0300

    EDA: estructura inicial e integración del módulo

---
+++ HelpEditorCode.js
@@ -0,0 +1,21 @@
+//
+// HelpEditorCode.js
+//
+
+function initHelpEditor(){
+
+    console.log("EDA inicializado");
+
+}
+
+function buildCommandList(){
+
+    console.log("Construyendo lista de comandos");
+
+}
+
+function selectCommand(cmd){
+
+    console.log("Comando seleccionado:",cmd);
+
+}
